00dbf3f5295d25f9befe9f3c068972cb161f4748
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-24 16:39:16 +0200

    Add some more code sample

--- src/Chapter2.purs
+++ src/Chapter2.purs
@@ -2,4 +2,16 @@ module Chapter2 where
 
 import Debug.Trace
 
-main = trace "Hello, World!"
+iAmANumber = 
+  let square x = x * x
+    in square 42
+
+iterate f 0 x = x
+iterate f n x = iterate f (n - 1) (f x)
+
+main = do
+  trace "Hello, World!"
+  print iAmANumber
+  print $ iterate ((+) 1) 10 0
+  
+
